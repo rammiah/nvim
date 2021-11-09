7a5dbc26da15c420cd73edb74954b6799cdbda47
rammiah <rammiahcn@gmail.com>
2021-11-09 17:48:38 +0800

    add save tmux

--- tmux/install_prefix_highlight.sh
+++
@@ -1,2 +0,0 @@
-test -e ~/.tmux || mkdir ~/.tmux
-git clone https://github.com/tmux-plugins/tmux-prefix-highlight.git ~/.tmux/tmux-prefix-highlight
---
+++ tmux/install_tmux_plugins.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+test -e ~/.tmux || mkdir ~/.tmux
+test -e ~/.tmux/tmux-prefix-highlight || git clone https://github.com/tmux-plugins/tmux-prefix-highlight.git ~/.tmux/tmux-prefix-highlight
+test -e ~/.tmux/tmux-resurrect || git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/tmux-resurrect
