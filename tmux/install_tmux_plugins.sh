1447678aac2f0936531a7733fdc7be6598a67255
wangyaning <wangyaning.ovo@bytedance.com>
2021-12-10 10:55:39 +0800

    add tmux-continuum

--- tmux/install_tmux_plugins.sh
+++ tmux/install_tmux_plugins.sh
@@ -2,3 +2,4 @@
 test -e ~/.tmux || mkdir ~/.tmux
 test -e ~/.tmux/tmux-prefix-highlight || git clone https://github.com/tmux-plugins/tmux-prefix-highlight.git ~/.tmux/tmux-prefix-highlight
 test -e ~/.tmux/tmux-resurrect || git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/tmux-resurrect
+test -e ~/.tmux/tmux-continuum || git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux/tmux-continuum
