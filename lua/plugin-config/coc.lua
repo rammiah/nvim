a93c45db4910db16d494b5c423b56f3d2f94c5f4
wangyaning <wangyaning.ovo@bytedance.com>
2022-03-22 11:10:57 +0800

    good

--- lua/plugin-config/coc.lua
+++ lua/plugin-config/coc.lua
@@ -1,9 +1,10 @@
 vim.cmd([[
 augroup mygroup
- "" autocmd!
+  autocmd!
   " Setup formatexpr specified filetype(s).
+  " autocmd FileType typescript,json setl formatexpr=CocAction('formatSelected')
   " Update signature help on jump placeholder.
-""  autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
+  autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
 augroup end
 
 " autocmd CursorHold * silent call CocActionAsync('highlight')
@@ -15,8 +16,8 @@ else
 endif
 " Make <CR> auto-select the first completion item and notify coc.nvim to
 " format on enter, <cr> could be remapped by other vim plugin
-" inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm()
-"                              \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+""inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm()
+""                             \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
 " Use `[g` and `]g` to navigate diagnostics
 " Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
 nmap <silent> [g <Plug>(coc-diagnostic-prev)
