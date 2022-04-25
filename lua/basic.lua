463b52df504a45caa9b15bdf34bb93e38680c735
wangyaning <wangyaning.ovo@bytedance.com>
2022-04-25 15:18:39 +0800

    good

--- lua/basic.lua
+++ lua/basic.lua
@@ -29,8 +29,8 @@ vim.o.cmdheight = 2
 -- 当文件被外部程序修改时，自动加载
 vim.o.autoread = true
 -- 禁止折行
-vim.o.wrap = false
-vim.wo.wrap = false
+vim.o.wrap = true
+vim.wo.wrap = true
 -- 行结尾可以跳到下一行
 vim.o.whichwrap = 'b,s,<,>,[,],h,l'
 -- 允许隐藏被修改过的buffer
@@ -40,7 +40,7 @@ vim.o.mouse = "nv"
 -- 禁止创建备份文件
 vim.o.backup = false
 vim.o.writebackup = false
-vim.o.swapfile = false
+vim.o.swapfile = true
 -- smaller updatetime
 vim.o.updatetime = 300
 -- 等待mappings
