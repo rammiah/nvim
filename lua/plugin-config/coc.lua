bff234dc2ed74eaae350e51820fe7d2f3acb3aae
rammiah <rammiahcn@gmail.com>
2022-06-01 23:57:31 +0800

    update coc

--- lua/plugin-config/coc.lua
+++ lua/plugin-config/coc.lua
@@ -1,36 +1,22 @@
 vim.cmd([[
-"augroup mygroup
-  "autocmd!
-  " Setup formatexpr specified filetype(s).
-  " autocmd FileType typescript,json setl formatexpr=CocAction('formatSelected')
-  " Update signature help on jump placeholder.
-  "autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
-"augroup end
 autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
 " autocmd CursorHold * silent call CocActionAsync('highlight')
-" Use <c-space> to trigger completion.
-if has('nvim')
-  inoremap <silent><expr> <c-space> coc#refresh()
-else
-  inoremap <silent><expr> <c-@> coc#refresh()
-endif
-" Make <CR> auto-select the first completion item and notify coc.nvim to
-" format on enter, <cr> could be remapped by other vim plugin
-""inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm()
-""                             \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+
 " Use `[g` and `]g` to navigate diagnostics
 " Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
 nmap <silent> [g <Plug>(coc-diagnostic-prev)
 nmap <silent> ]g <Plug>(coc-diagnostic-next)
-
-" autocmd BufWritePre *.go call CocAction('runCommand', 'editor.action.organizeImport')
-" autocmd BufWritePre *.go :call CocAction('runCommand', 'editor.action.formatDocument') | call CocAction('runCommand', 'editor.action.organizeImport')
-" Add `:Format` command to format current buffer.
-"" ommand! -nargs=0 Format :call CocAction('format')
-
-" Add `:Fold` command to fold current buffer.
-" command! -nargs=? Fold :call CocAction('fold', <f-args>)
-
-" Add `:OR` command for organize imports of the current buffer.
-" command! -nargs=0 OR   :call CocAction('runCommand', 'editor.action.organizeImport')
 ]])
+
+vim.g.coc_global_extensions = {
+    'coc-go',
+    'coc-spell-checker',
+    'coc-lua',
+    'coc-json',
+    'coc-yaml',
+    'coc-tsserver',
+    'coc-vimlsp',
+    'coc-pyright',
+    'coc-highlight',
+}
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,46 +1,48 @@
 local use = require("packer").use
 return require('packer').startup(function()
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  -- color theme
-  use {'ellisonleao/gruvbox.nvim', requires = {'rktjmp/lush.nvim'}}
-  use 'overcache/NeoSolarized'
-  use 'NLKNguyen/papercolor-theme'
-  use 'mrtazz/molokai.vim'
-  use 'rakr/vim-one'
-  -- nerd tree
-  use { 'preservim/nerdtree', requires = {'Xuyuanp/nerdtree-git-plugin'} }
-  -- dev icons
-  use 'ryanoasis/vim-devicons'
-  -- treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', requires = 'p00f/nvim-ts-rainbow'}
-  -- commenter
-  use 'preservim/nerdcommenter'
-  -- bufferline (新增)
-  use { 'akinsho/bufferline.nvim', tag = "*", requires = {'kyazdani42/nvim-web-devicons'} }
-  -- vim-go
-  -- use 'fatih/vim-go'
-  use{ 'rammiah/nvim-go', requires = {'nvim-lua/plenary.nvim', 'rcarriga/nvim-notify'} }
-  use 'rammiah/gocommand.vim'
-  -- coc.nvim
-  use { 'rammiah/coc.nvim', branch = 'master', run = 'yarn install --frozen-lockfile'}
-  -- air-line
-  use { 'vim-airline/vim-airline', requires = {'vim-airline/vim-airline-themes'} }
-  -- rainbow
-  -- use { 'luochen1990/rainbow' }
-  -- git
-  use { 'airblade/vim-gitgutter' }
-  use 'tpope/vim-fugitive'
-  -- lexima
-  use { 'cohama/lexima.vim' }
-  -- surround
-  use 'tpope/vim-surround'
-  use 'tpope/vim-repeat'
-  -- blamer
-  -- use 'APZelos/blamer.nvim'
-  use { 'ojroques/vim-oscyank', branch= 'main' }
-  -- suda
-  use 'lambdalisue/suda.vim'
-  use 'nvim-telescope/telescope.nvim'
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    -- color theme
+    use {'ellisonleao/gruvbox.nvim', requires = {'rktjmp/lush.nvim'}}
+    use 'overcache/NeoSolarized'
+    use 'NLKNguyen/papercolor-theme'
+    use 'mrtazz/molokai.vim'
+    use 'rakr/vim-one'
+    -- nerd tree
+    use { 'preservim/nerdtree', requires = {'Xuyuanp/nerdtree-git-plugin'} }
+    -- dev icons
+    use 'ryanoasis/vim-devicons'
+    -- treesitter
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', requires = 'p00f/nvim-ts-rainbow'}
+    -- commenter
+    use 'preservim/nerdcommenter'
+    -- bufferline (新增)
+    use { 'akinsho/bufferline.nvim', tag = "*", requires = {'kyazdani42/nvim-web-devicons'} }
+    -- vim-go
+    -- use 'fatih/vim-go'
+    use{ 'rammiah/nvim-go', requires = {'nvim-lua/plenary.nvim', 'rcarriga/nvim-notify'} }
+    use 'rammiah/gocommand.vim'
+    -- coc.nvim
+    use { 'rammiah/coc.nvim', branch = 'master', run = 'yarn install --frozen-lockfile'}
+    -- air-line
+    use { 'vim-airline/vim-airline', requires = {'vim-airline/vim-airline-themes'} }
+    -- rainbow
+    -- use { 'luochen1990/rainbow' }
+    -- git
+    use { 'airblade/vim-gitgutter' }
+    use 'tpope/vim-fugitive'
+    -- lexima
+    use { 'cohama/lexima.vim' }
+    -- surround
+    use 'tpope/vim-surround'
+    use 'tpope/vim-repeat'
+    -- blamer
+    -- use 'APZelos/blamer.nvim'
+    use { 'ojroques/vim-oscyank', branch= 'main' }
+    -- suda
+    use 'lambdalisue/suda.vim'
+
+    -- telescope and plugins
+    use 'nvim-telescope/telescope.nvim'
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 end)
