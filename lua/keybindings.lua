597d6c7592eb01edb5b67d05b75934c9cadd401c
rammiah <rammiahcn@gmail.com>
2022-06-04 00:51:06 +0800

    add keymaps

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -15,41 +15,11 @@ map("n", "<A-l>", "<C-w>l")
 map('n', '<C-n>', ':NERDTreeToggle<CR>')
 map('n', '<C-f>', ':NERDTreeFind<CR>')
 -- bufferline 左右Tab切换
--- map("n", "<leader>tn", ":tabnext<CR>", opt)
--- map("n", "<leader>tp", ":tabprevious<CR>", opt)
+map("n", "]t", ":tabnext<CR>")
+map("n", "[t", ":tabprevious<CR>")
+map("n", "]q", ":cnext<CR>")
+map("n", "[q", ":cprevious<CR>")
 -- nvim-treesitter 代码格式化
-
-vim.cmd([[
-" Use K to show documentation in preview window.
-nnoremap <silent> K :call ShowDocumentation()<CR>
-
-function! ShowDocumentation()
-  if CocHasProvider('hover')
-    call CocActionAsync('doHover')
-  else
-    call feedkeys('K', 'in')
-  endif
-endfunction
-
-
-" map for format key use editor.action.formatDocument
-nnoremap <silent> <leader>i :call FormatDocument()<CR>
-function! FormatDocument()
-    if CocHasProvider('format')
-        call CocActionAsync('format')
-    else
-        call feedkeys('gg=G', 'in')
-    endif
-endfunction
-]])
-
-map("n", "<leader>rn", ":call CocActionAsync('rename')<CR>")
-map("n", "<space>a", ":CocList diagnostics<CR>")
-map("n", "<space>o", ":CocList outline<CR>")
-map("n", "gd", ":call CocActionAsync('jumpDefinition')<CR>")
-map("n", "gt", ":call CocActionAsync('jumpTypeDefinition')<CR>")
-map("n", "gi", ":call CocActionAsync('jumpImplementation')<CR>")
-map("n", "gr", ":call CocActionAsync('jumpReferences')<CR>")
 
 -- tag
 map("n", "<leader>tag", ":!ctags -R<CR>", { noremap = true })
@@ -58,8 +28,8 @@ map("n", "<leader>tag", ":!ctags -R<CR>", { noremap = true })
 map("v", "<leader>cp", ":OSCYank<CR>")
 
 -- githunk jump
-map("n", ']h', '<Plug>(GitGutterNextHunk)')
-map('n', '[h', '<Plug>(GitGutterPrevHunk)')
+-- map("n", ']h', '<Plug>(GitGutterNextHunk)')
+-- map('n', '[h', '<Plug>(GitGutterPrevHunk)')
 
 local pluginKeys = {}
 
