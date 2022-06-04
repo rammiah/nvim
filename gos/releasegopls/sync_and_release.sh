c656d3f2c7874f435eb8d1b48518a27798608382
rammiah <rammiahcn@gmail.com>
2022-06-05 01:40:30 +0800

    add repo sync

--- gos/releasegopls/sync_and_release.sh
+++ gos/releasegopls/sync_and_release.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+set -x
 cd /home/rammiah/github/tools
 
 git checkout -- .
+
+gh repo sync
+
 git apply tsprotocol.patch
 
 ./gopls/release.sh
 
 git checkout -- .
+
+set +x
