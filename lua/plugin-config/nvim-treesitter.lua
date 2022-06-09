8987784ae74a57506e28dfa37e944cd061b63777
rammiah <rammiahcn@gmail.com>
2022-06-10 01:10:29 +0800

    add treesitter-textobjects

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -76,6 +76,46 @@ require('nvim-treesitter.configs').setup {
     },
     auto_tag = {
         enable = true,
+    },
+    textobjects = {
+        select = {
+            enable = true,
+
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+                ["al"] = "@loop.outer",
+                ["il"] = "@loop.inner",
+                ["cc"] = "@comment.outer",
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                -- ["]m"] = "@function.outer",
+                ["]]"] = "@function.outer",
+                -- ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+                -- ["]M"] = "@function.outer",
+                -- ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+                ["[["] = "@function.outer",
+                -- ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+                -- ["[M"] = "@function.outer",
+                -- ["[]"] = "@class.outer",
+            },
+        },
     }
 }
 -- 开启 Folding
