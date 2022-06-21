4511b36aa7035955d050f08f803d3c0851a9a7c3
wangyaning <wangyaning.ovo@bytedance.com>
2022-06-21 10:42:22 +0800

    icon width

--- lua/plugin-config/lualine.lua
+++ lua/plugin-config/lualine.lua
@@ -87,7 +87,7 @@ require("lualine").setup {
                     info  = "DiagnosticInfo", -- Changes diagnostics' info color.
                     hint  = "DiagnosticHint", -- Changes diagnostics' hint color.
                 },
-                symbols = { error = "", warn = "ﴞ", info = "", hint = "ﯧ" },
+                symbols = { error = " ", warn = "ﴞ ", info = " ", hint = "ﯧ " },
                 colored = true, -- Displays diagnostics status in color if set to true.
                 update_in_insert = false, -- Update diagnostics in insert mode.
                 always_visible = false, -- Show diagnostics even if there are none.
