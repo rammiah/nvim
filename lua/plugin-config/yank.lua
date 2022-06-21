3cee03a503586c1b9e272ccf3145c4e0597630fc
rammiah <rammiahcn@gmail.com>
2022-06-22 00:47:02 +0800

    add oscyank autocmd

---
+++ lua/plugin-config/yank.lua
@@ -0,0 +1,4 @@
+vim.cmd[[
+autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankReg "' | endif
+let g:oscyank_silent = v:true  " or 1 for older versions of Vim
+]]
