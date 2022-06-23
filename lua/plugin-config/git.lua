569db8e8a838694659f52edd83905e4646cbf7e6
wangyaning <wangyaning.ovo@bytedance.com>
2022-06-23 17:00:55 +0800

    fix cocPrev

--- lua/plugin-config/git.lua
+++ lua/plugin-config/git.lua
@@ -6,7 +6,7 @@ require("git").setup({
         browse = false, -- Open file/folder in git repository
         open_pull_request = false, -- Open pull request of the current branch
         create_pull_request = false, -- Create a pull request with the target branch is set in the `target_branch` option
-        diff = "<leader>gd", -- Opens a new diff that compares against the current index
+        diff = false, -- "<leader>gd", -- Opens a new diff that compares against the current index
         diff_close = "q", -- Close git diff
         revert = false, -- Revert to the specific commit
         revert_file = false, -- Revert the current file to the specific commit
