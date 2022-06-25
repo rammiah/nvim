290de482d875ecd092b4f60e374ad11a72b93f72
rammiah <rammiahcn@gmail.com>
2022-06-25 18:50:48 +0800

    packer.nvim: git config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -111,6 +111,25 @@ return require("packer").startup({
         transitive_opt       = true, -- Make dependencies of opt plugins also opt by default
         transitive_disable   = true, -- Automatically disable dependencies of disabled plugins
         auto_reload_compiled = true, -- Automatically reload the compiled file after creating it.
+        git                  = {
+            cmd = "git", -- The base command for git operations
+            subcommands = { -- Format strings for git subcommands
+                update         = "pull --ff-only --progress --rebase=false --tags --force",
+                install        = "clone --depth %i --no-single-branch --progress",
+                fetch          = "fetch --depth 999999 --progress",
+                checkout       = "checkout %s --",
+                update_branch  = "merge --ff-only @{u}",
+                current_branch = "branch --show-current",
+                diff           = "log --color=never --pretty=format:FMT --no-show-signature HEAD@{1}...HEAD",
+                diff_fmt       = "%%h %%s (%%cr)",
+                get_rev        = "rev-parse --short HEAD",
+                get_msg        = "log --color=never --pretty=format:FMT --no-show-signature HEAD -n 1",
+                submodules     = "submodule update --init --recursive --progress"
+            },
+            depth = 1, -- Git clone depth
+            clone_timeout = 60, -- Timeout, in seconds, for git clones
+            default_url_format = 'https://github.com/%s' -- Lua format string used for "aaa/bbb" style plugins
+        },
         display              = {
             open_fn = require("packer.util").float,
             working_sym = "⟳", -- The symbol for a plugin being installed/updated
