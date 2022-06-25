cd296265f484c1ef55d80ddbc470fe7c5122ca70
rammiah <rammiahcn@gmail.com>
2022-06-25 19:05:45 +0800

    plugin: add glow.nvim

---
+++ lua/plugin-config/glow.lua
@@ -0,0 +1,8 @@
+if not require("local-util").safe_load("glow") then
+    return
+end
+
+vim.g.glow_border = "rounded"
+vim.g.glow_width = 120
+vim.g.glow_use_pager = true
+vim.g.glow_style = "light"
