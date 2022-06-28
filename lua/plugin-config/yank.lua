3f705671600454614d562fabdf40ea2585f10502
wangyaning <wangyaning.ovo@bytedance.com>
2022-06-28 19:48:34 +0800

    yank: tmux compatibility

--- lua/plugin-config/yank.lua
+++ lua/plugin-config/yank.lua
@@ -1,4 +1,5 @@
 vim.cmd[[
 autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankReg "' | endif
 let g:oscyank_silent = v:true  " or 1 for older versions of Vim
+let g:oscyank_term = 'default'
 ]]
