c2508895ed3b7bd25a9259e3c6dcb584d42d9f01
rammiah <rammiahcn@gmail.com>
2022-07-07 01:09:19 +0800

    add hop

---
+++ lua/plugin-config/hop.lua
@@ -0,0 +1,14 @@
+if not require("local-util").safe_load("hop") then
+    return
+end
+local hint = require("hop.hint")
+local map = require("local-util").KeyMap
+map("n", "<C-h>", "<Cmd>HopWord<CR>")
+-- you can configure Hop the way you like here; see :h hop-config
+require "hop".setup {
+    keys = "etovxqpdygfblzhckisuran",
+    multi_windows = false,
+    hint_position = hint.HintPosition.BEGIN,
+    uppercase_labels = false,
+}
+
