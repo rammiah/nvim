9b9a1136764c08f0c1ff7de81347f69f6759308f
rammiah <rammiahcn@gmail.com>
2022-07-10 17:22:46 +0800

    add tmux

--- tmux/install_tmux_plugins.sh
+++ tmux/install_tmux_plugins.sh
@@ -3,3 +3,4 @@ test -e ~/.tmux || mkdir ~/.tmux
 test -e ~/.tmux/tmux-prefix-highlight || git clone https://github.com/tmux-plugins/tmux-prefix-highlight.git ~/.tmux/tmux-prefix-highlight
 test -e ~/.tmux/tmux-resurrect || git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/tmux-resurrect
 test -e ~/.tmux/tmux-continuum || git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux/tmux-continuum
+test -e ~/.tmux/tmux-fzf || git clone https://github.com/sainnhe/tmux-fzf ~/.tmux/tmux-fzf
