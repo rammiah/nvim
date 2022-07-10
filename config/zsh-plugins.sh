3fcf63e1d5885dbca80863a87ca03acf15136e22
rammiah <rammiahcn@gmail.com>
2022-07-10 17:22:46 +0800

    add tmux

--- config/zsh-plugins.sh
+++ config/zsh-plugins.sh
@@ -1,2 +1,10 @@
-test -e ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#!/usr/bin/env bash
 
+test -e ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || \
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+test -e ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions || \
+    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+
+test -e ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab || \
+    git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
