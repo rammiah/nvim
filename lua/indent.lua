58b5fe49c34d367fdddcce2565dee07203d0b697
rammiah <rammiahcn@gmail.com>
2022-07-12 00:12:45 +0800

    add ftplugin

--- lua/indent.lua
+++ lua/indent.lua
@@ -6,11 +6,4 @@ set shiftwidth=4
 set tabstop=4
 set expandtab
 set softtabstop=4
-" only not expand tab for golang
-autocmd FileType go setlocal noexpandtab
-" expandtab for other files
-autocmd FileType python,json,yaml setlocal shiftwidth=2 softtabstop=2
-" autocmd FileType thrift,c,cpp setlocal shiftwidth=4 tabstop=4 softtabstop=4
-autocmd FileType Caddyfile setlocal noexpandtab
-autocmd FileType gitconfig setlocal noexpandtab
 ]])
