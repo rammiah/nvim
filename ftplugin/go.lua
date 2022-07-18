e49eb685d98794cd3dadb318bbb71ac2b4697f36
rammiah <rammiahcn@gmail.com>
2022-07-18 16:54:17 +0800

    dap: add python and go

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -1,3 +1,7 @@
-vim.bo.shiftwidth=4
-vim.bo.softtabstop=4
-vim.bo.expandtab=false
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+vim.bo.expandtab = false
+
+local save_load = require("local-util").safe_load
+
+save_load("dap-go")
---
+++ lua/plugin-config/dap.lua
@@ -0,0 +1,30 @@
+if not require("local-util").safe_load("dap") then
+    return
+end
+
+local dap = require("dap")
+
+-- -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+-- dap.configurations.go = {
+--   {
+--     type = "delve",
+--     name = "Debug",
+--     request = "launch",
+--     program = "${file}"
+--   },
+--   {
+--     type = "delve",
+--     name = "Debug test", -- configuration for debugging test files
+--     request = "launch",
+--     mode = "test",
+--     program = "${file}"
+--   },
+--   -- works with go.mod packages and sub packages 
+--   {
+--     type = "delve",
+--     name = "Debug test (go.mod)",
+--     request = "launch",
+--     mode = "test",
+--     program = "./${relativeFileDirname}"
+--   } 
+-- }
