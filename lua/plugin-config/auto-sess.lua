a19a14c8b23fdd6a5cb422a50f19d052ab373694
rammiah <rammiahcn@gmail.com>
2022-07-19 23:19:06 +0800

    hydra,auto-sess

--- lua/plugin-config/auto-sess.lua
+++ lua/plugin-config/auto-sess.lua
@@ -7,7 +7,7 @@ require("auto-session").setup {
     auto_session_enable_last_session = false,
     auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
     auto_session_enabled = true,
-    auto_save_enabled = true,
+    auto_save_enabled = false,
     auto_restore_enabled = false,
     auto_session_suppress_dirs = { "~/" },
     auto_session_use_git_branch = nil,
