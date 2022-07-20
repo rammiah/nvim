9942b9885d378b0375ae9754105c8e4a8f46d4f3
rammiah <rammiahcn@gmail.com>
2022-07-20 20:34:40 +0800

    modify icons

--- lua/plugin-config/bufferline.lua
+++ lua/plugin-config/bufferline.lua
@@ -22,7 +22,7 @@ require("bufferline").setup {
         -- as an escape hatch for people who cannot bear it for whatever reason
         indicator_icon = "▎",
         buffer_close_icon = "",
-        modified_icon = "",
+        modified_icon = "●",
         close_icon = "",
         left_trunc_marker = "",
         right_trunc_marker = "",
