ace127745d1203c5ca3baabbbd7f5352f61a2da8
rammiah <rammiahcn@gmail.com>
2022-07-20 14:32:07 +0800

    packer: change icons

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -202,10 +202,10 @@ return require("packer").startup({
         },
         display              = {
             open_fn = require("packer.util").float,
-            working_sym = "⟳", -- The symbol for a plugin being installed/updated
+            working_sym = "", -- The symbol for a plugin being installed/updated
             error_sym = "✗", -- The symbol for a plugin with an error in installation/updating
             done_sym = "✓", -- The symbol for a plugin which has completed installation/updating
-            removed_sym = "-", -- The symbol for an unused plugin which was removed
+            removed_sym = "", -- The symbol for an unused plugin which was removed
             moved_sym = "→", -- The symbol for a plugin which was moved (e.g. from opt to start)
             header_sym = "━", -- The symbol for the header line in packer's display
             show_all_info = true, -- Should packer show all update details automatically?
