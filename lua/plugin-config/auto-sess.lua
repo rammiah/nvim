1b55aeb67d8ef801f2ecb98bcd8210cb25106bb5
rammiah <rammiahcn@gmail.com>
2022-07-20 16:32:34 +0800

    session: add session

--- lua/plugin-config/auto-sess.lua
+++ lua/plugin-config/auto-sess.lua
@@ -2,6 +2,8 @@ if not require("local-util").safe_load("auto-session") then
     return
 end
 
+local map = require("local-util").KeyMap
+
 require("auto-session").setup {
     log_level = "info",
     auto_session_enable_last_session = false,
@@ -14,3 +16,6 @@ require("auto-session").setup {
     -- the configs below are lua only
     bypass_session_save_file_types = { "NvimTree", "thrift" }
 }
+
+map("n", "<leader>ss", "<Cmd>SaveSession<CR>")
+map("n", "<leader>sr", "<Cmd>RestoreSession<CR>")
