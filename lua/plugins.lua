59e4c148e60e40c3cc003458dfd88c794134d43a
rammiah <rammiahcn@gmail.com>
2022-07-21 22:36:13 +0800

    remove repeat

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -53,9 +53,7 @@ return require("packer").startup({
         use "windwp/nvim-autopairs"
         use "windwp/nvim-ts-autotag"
         -- surround
-        -- use "tpope/vim-surround"
         use "kylechui/nvim-surround"
-        use "tpope/vim-repeat"
         use { "ojroques/vim-oscyank", branch = "main" }
         -- suda sudo write
         use "lambdalisue/suda.vim"
