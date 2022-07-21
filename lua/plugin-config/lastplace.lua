bc0f8eaf1f8e8aca5ae2596279f76f3d66c7c7c0
rammiah <rammiahcn@gmail.com>
2022-07-21 21:56:41 +0800

    add c, d,

--- lua/plugin-config/lastplace.lua
+++ lua/plugin-config/lastplace.lua
@@ -3,7 +3,7 @@ if not require("local-util").safe_load("nvim-lastplace") then
 end
 
 require "nvim-lastplace".setup {
-    lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+    lastplace_ignore_buftype = { "quickfix", "nofile", "help", "NvimTree" },
     lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit", "NvimTree" },
     lastplace_open_folds = true
 }
