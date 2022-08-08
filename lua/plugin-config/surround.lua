afba386be0aae40b0fd66a13fb18501bfb0d2d84
rammiah <rammiahcn@gmail.com>
2022-08-08 11:42:26 +0800

    update config

--- lua/plugin-config/surround.lua
+++ lua/plugin-config/surround.lua
@@ -2,15 +2,7 @@ if not require("local-util").safe_load("nvim-surround") then
     return
 end
 
--- Since `vim.fn.input()` does not handle keyboard interrupts, we use a protected call to check whether the user has
--- used `<C-c>` to cancel the input. This is not needed if `<Esc>` is used to cancel the input.
-local get_input = function(prompt)
-    local ok, result = pcall(vim.fn.input, { prompt = prompt })
-    if not ok then
-        return nil
-    end
-    return result
-end
+local M = require("nvim-surround.config")
 
 require("nvim-surround").setup({
     keymaps = { -- vim-surround style keymaps
@@ -25,38 +17,231 @@ require("nvim-surround").setup({
         visual = "S",
         visual_line = "gS",
     },
-    delimiters = {
-        pairs = {
-            ["("] = { "( ", " )" },
-            [")"] = { "(", ")" },
-            ["{"] = { "{ ", " }" },
-            ["}"] = { "{", "}" },
-            ["<"] = { "< ", " >" },
-            [">"] = { "<", ">" },
-            ["["] = { "[ ", " ]" },
-            ["]"] = { "[", "]" },
-            [","] = { ",", "," },
-            -- Define pairs based on function evaluations!
-            ["i"] = function()
-                local left_delimiter = get_input("Enter the left delimiter: ")
-                if left_delimiter then
-                    local right_delimiter = get_input("Enter the right delimiter: ")
-                    if right_delimiter then
-                        return { left_delimiter, right_delimiter }
-                    end
-                end
+    surrounds = {
+        ["("] = {
+            add = { "( ", " )" },
+            find = function()
+                return M.get_selection({ textobject = "(" })
             end,
-            ["f"] = function()
-                local result = get_input("Enter the function name: ")
-                if result then
-                    return { result .. "(", ")" }
-                end
-            end,
+            delete = "^(. ?)().-( ?.)()$",
+            change = {
+                target = "^(. ?)().-( ?.)()$",
+            },
         },
-        separators = {
-            ["'"] = { "'", "'" },
-            ['"'] = { '"', '"' },
-            ["`"] = { "`", "`" },
+        [")"] = {
+            add = { "(", ")" },
+            find = function()
+                return M.get_selection({ textobject = ")" })
+            end,
+            delete = "^(.)().-(.)()$",
+            change = {
+                target = "^(.)().-(.)()$",
+            },
+        },
+        ["{"] = {
+            add = { "{ ", " }" },
+            find = function()
+                return M.get_selection({ textobject = "{" })
+            end,
+            delete = "^(. ?)().-( ?.)()$",
+            change = {
+                target = "^(. ?)().-( ?.)()$",
+            },
+        },
+        ["}"] = {
+            add = { "{", "}" },
+            find = function()
+                return M.get_selection({ textobject = "}" })
+            end,
+            delete = "^(.)().-(.)()$",
+            change = {
+                target = "^(.)().-(.)()$",
+            },
+        },
+        ["<"] = {
+            add = { "< ", " >" },
+            find = function()
+                return M.get_selection({ textobject = "<" })
+            end,
+            delete = "^(. ?)().-( ?.)()$",
+            change = {
+                target = "^(. ?)().-( ?.)()$",
+            },
+        },
+        [">"] = {
+            add = { "<", ">" },
+            find = function()
+                return M.get_selection({ textobject = ">" })
+            end,
+            delete = "^(.)().-(.)()$",
+            change = {
+                target = "^(.)().-(.)()$",
+            },
+        },
+        ["["] = {
+            add = { "[ ", " ]" },
+            find = function()
+                return M.get_selection({ textobject = "[" })
+            end,
+            delete = "^(. ?)().-( ?.)()$",
+            change = {
+                target = "^(. ?)().-( ?.)()$",
+            },
+        },
+        ["]"] = {
+            add = { "[", "]" },
+            find = function()
+                return M.get_selection({ textobject = "]" })
+            end,
+            delete = "^(.)().-(.)()$",
+            change = {
+                target = "^(.)().-(.)()$",
+            },
+        },
+        ["'"] = {
+            add = { "'", "'" },
+            find = function()
+                return M.get_selection({ textobject = "'" })
+            end,
+            delete = "^(.)().-(.)()$",
+            change = {
+                target = "^(.)().-(.)()$",
+            },
+        },
+        ['"'] = {
+            add = { '"', '"' },
+            find = function()
+                return M.get_selection({ textobject = '"' })
+            end,
+            delete = "^(.)().-(.)()$",
+            change = {
+                target = "^(.)().-(.)()$",
+            },
+        },
+        ["`"] = {
+            add = { "`", "`" },
+            find = function()
+                return M.get_selection({ textobject = "`" })
+            end,
+            delete = "^(.)().-(.)()$",
+            change = {
+                target = "^(.)().-(.)()$",
+            },
+        },
+        ["i"] = { -- TODO: Add find/delete/change functions
+            add = function()
+                local left_delimiter = M.get_input("Enter the left delimiter: ")
+                local right_delimiter = left_delimiter and M.get_input("Enter the right delimiter: ")
+                if right_delimiter then
+                    return { { left_delimiter }, { right_delimiter } }
+                end
+            end,
+            find = function() end,
+            delete = function() end,
+            change = { target = function() end },
+        },
+        ["t"] = {
+            add = function()
+                local input = M.get_input("Enter the HTML tag: ")
+                if input then
+                    local element = input:match("^<?([%w-]*)")
+                    local attributes = input:match("%s+([^>]+)>?$")
+
+                    local open = attributes and element .. " " .. attributes or element
+                    local close = element
+
+                    return { { "<" .. open .. ">" }, { "</" .. close .. ">" } }
+                end
+            end,
+            find = function()
+                return M.get_selection({ textobject = "t" })
+            end,
+            delete = "^(%b<>)().-(%b<>)()$",
+            change = {
+                target = "^<([%w-]*)().-([^/]*)()>$",
+                replacement = function()
+                    local element = M.get_input("Enter the HTML element: ")
+                    if element then
+                        return { { element }, { element } }
+                    end
+                end,
+            },
+        },
+        ["T"] = {
+            add = function()
+                local input = M.get_input("Enter the HTML tag: ")
+                if input then
+                    local element = input:match("^<?([%w-]*)")
+                    local attributes = input:match("%s+([^>]+)>?$")
+
+                    local open = attributes and element .. " " .. attributes or element
+                    local close = element
+
+                    return { { "<" .. open .. ">" }, { "</" .. close .. ">" } }
+                end
+            end,
+            find = function()
+                return M.get_selection({ textobject = "t" })
+            end,
+            delete = "^(%b<>)().-(%b<>)()$",
+            change = {
+                target = "^<([^>]*)().-([^%/]*)()>$",
+                replacement = function()
+                    local input = M.get_input("Enter the HTML tag: ")
+                    if input then
+                        local element = input:match("^<?([%w-]*)")
+                        local attributes = input:match("%s+([^>]+)>?$")
+
+                        local open = attributes and element .. " " .. attributes or element
+                        local close = element
+
+                        return { { open }, { close } }
+                    end
+                end,
+            },
+        },
+        ["f"] = {
+            add = function()
+                local result = M.get_input("Enter the function name: ")
+                if result then
+                    return { { result .. "(" }, { ")" } }
+                end
+            end,
+            find = "[%w%-_:.>]+%b()",
+            delete = "^([%w%-_:.>]+%()().-(%))()$",
+            change = {
+                target = "^.-([%w_]+)()%b()()()$",
+                replacement = function()
+                    local result = M.get_input("Enter the function name: ")
+                    if result then
+                        return { { result }, { "" } }
+                    end
+                end,
+            },
+        },
+        invalid_key_behavior = {
+            add = function(char)
+                return { { char }, { char } }
+            end,
+            find = function(char)
+                return M.get_selection({
+                    pattern = vim.pesc(char) .. ".-" .. vim.pesc(char),
+                })
+            end,
+            delete = function(char)
+                return M.get_selections({
+                    char = char,
+                    pattern = "^(.)().-(.)()$",
+                })
+            end,
+            change = {
+                target = function(char)
+                    return M.get_selections({
+                        char = char,
+                        pattern = "^(.)().-(.)()$",
+                    })
+                end,
+            },
         },
         HTML = {
             ["t"] = "type",
@@ -72,7 +257,7 @@ require("nvim-surround").setup({
             ["s"] = { ")", "]", "}", ">", "'", '"', "`" }, -- Any surrounding delimiter
         },
     },
-    highlight_motion = { -- Highlight text-objects before surrounding them
+    highlight = { -- Highlight text-objects before surrounding them
         duration = 0,
     },
     move_cursor = "begin",
