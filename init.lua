df8d4a67c975839660a14cd752b0c2d71bed2e46
rammiah <rammiahcn@gmail.com>
2022-08-13 13:48:43 +0800

    add command

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 -- basic config
 require("basic")
+require("commands")
 require("keybindings")
 require("indent")
 
