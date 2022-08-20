3d7f38170fdc15b962c73c5764e6bcb4ec37ab7d
rammiah <rammiahcn@gmail.com>
2022-08-20 15:11:57 +0800

    update commentstring hook

--- lua/plugin-config/comment.lua
+++ lua/plugin-config/comment.lua
@@ -65,24 +65,7 @@ require("Comment").setup {
 
     ---Pre-hook, called before commenting the line
     ---@type fun(ctx: CommentCtx):string
-    pre_hook = function(ctx)
-        if not require("local-util").safe_load("ts_context_commentstring") then
-            return
-        end
-        local U = require "Comment.utils"
-
-        local location = nil
-        if ctx.ctype == U.ctype.block then
-            location = require("ts_context_commentstring.utils").get_cursor_location()
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-            location = require("ts_context_commentstring.utils").get_visual_start_location()
-        end
-
-        return require("ts_context_commentstring.internal").calculate_commentstring {
-            key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-            location = location,
-        }
-    end,
+    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
 
     ---Post-hook, called after commenting is done
     ---@type fun(ctx: CommentCtx)
