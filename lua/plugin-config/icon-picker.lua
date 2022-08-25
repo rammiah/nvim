bde75b6db0b9641de3c06190f5b0aafa03ef3468
rammiah <rammiahcn@gmail.com>
2022-08-25 14:05:30 +0800

    remove icon-picker

--- lua/plugin-config/icon-picker.lua
+++
@@ -1,9 +0,0 @@
-if not require("local-util").safe_load("icon-picker") then
-    return
-end
-
-local map = require("local-util").KeyMap
-require("icon-picker")
-
--- map("i", "<C-i>", "<cmd>PickIconsInsert<cr>")
-map("n", "<leader><leader>i", "<cmd>PickIcons<cr>")
