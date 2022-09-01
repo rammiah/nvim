183f32be18ddab4cb0ce209321205598c8e5885d
rammiah <rammiahcn@gmail.com>
2022-09-01 10:45:33 +0800

    git: update gitsigns, git

--- lua/plugin-config/git.lua
+++ lua/plugin-config/git.lua
@@ -4,16 +4,17 @@ end
 
 require("git").setup({
     keymaps = {
+        custom_only = true,
         blame = "<leader>gb", -- Open blame window
         quit_blame = "q", -- Close blame window
         blame_commit = "<CR>", -- Open blame commit
-        browse = false, -- Open file/folder in git repository
-        open_pull_request = false, -- Open pull request of the current branch
-        create_pull_request = false, -- Create a pull request with the target branch is set in the `target_branch` option
-        diff = false, -- "<leader>gd", -- Opens a new diff that compares against the current index
+        browse = nil, -- Open file/folder in git repository
+        open_pull_request = nil, -- Open pull request of the current branch
+        create_pull_request = nil, -- Create a pull request with the target branch is set in the `target_branch` option
+        diff = "<leader>gd", -- "<leader>gd", -- Opens a new diff that compares against the current index
         diff_close = "q", -- Close git diff
-        revert = false, -- Revert to the specific commit
-        revert_file = false, -- Revert the current file to the specific commit
+        revert = nil, -- Revert to the specific commit
+        revert_file = nil, -- Revert the current file to the specific commit
     },
     -- Default target branch when create a pull request
     target_branch = "master",
