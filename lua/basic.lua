578a45ad87f25e4254054fe2952cabb982246959
rammiah <rammiahcn@gmail.com>
2022-09-03 11:01:07 +0800

    neovide font

--- lua/basic.lua
+++ lua/basic.lua
@@ -88,9 +88,10 @@ vim.api.nvim_exec('language en_US.UTF-8', true)
 -- lazyredraw
 vim.o.lazyredraw = true
 -- number add or sub format recognize
-vim.o.nrformats="alpha,bin,hex,unsigned"
+vim.o.nrformats = "alpha,bin,hex,unsigned"
 -- history length
 vim.o.history = 1000
-vim.o.clipboard = vim.o.clipboard.."unnamedplus"
+vim.o.clipboard = vim.o.clipboard .. "unnamedplus"
 vim.o.guicursor = [[i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff500-blinkon500,sm:block-blinkwait175-blinkoff150-blinkon175]]
-vim.o.guifont="JetBrainsMono Nerd Font"
+vim.o.guifont = "JetBrainsMonoNL Nerd Font"
+vim.o.linespace = 0
