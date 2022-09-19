87f4321a3d303d370b6aee24358c7108527145e1
rammiah <rammiahcn@gmail.com>
2022-09-19 17:49:56 +0800

    add tmux mouse support

--- config/tmux/install_tmux_plugins.sh
+++ config/tmux/install_tmux_plugins.sh
@@ -4,3 +4,4 @@ test -e ~/.tmux/tmux-prefix-highlight || git clone https://github.com/tmux-plugi
 test -e ~/.tmux/tmux-resurrect || git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/tmux-resurrect
 test -e ~/.tmux/tmux-continuum || git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux/tmux-continuum
 test -e ~/.tmux/tmux-fzf || git clone https://github.com/sainnhe/tmux-fzf ~/.tmux/tmux-fzf
+test -e ~/.tmux/tmux-better-mouse-mode || git clone https://github.com/nhdaly/tmux-better-mouse-mode ~/.tmux/tmux-better-mouse-mode
