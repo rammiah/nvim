965b2de8e1a7162b30fca9ffff33de7c90bac4c8
rammiah <rammiahcn@gmail.com>
2022-09-26 22:50:48 +0800

    clean init.lua

--- lua/plugin-config/yank.lua
+++ lua/plugin-config/yank.lua
@@ -1,5 +1,1 @@
-vim.cmd[[
-autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankReg "' | endif
-let g:oscyank_silent = v:true  " or 1 for older versions of Vim
-let g:oscyank_term = 'default'
-]]
+
