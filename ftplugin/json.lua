68da207aeba49bb5fd79a78a041c0ab613f7db84
rammiah <rammiahcn@gmail.com>
2022-10-10 18:53:12 +0800

    add jsonpath

--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -1,2 +1,9 @@
+local map = require("localutils").KeyMap
 vim.bo.shiftwidth=2
 vim.bo.softtabstop=2
+if vim.fn.exists("+winbar") == 1 then
+    vim.wo.winbar = "%{luaeval('require\"jsonpath\".get()')}"
+end
+
+-- send json path to clipboard
+map("n", "y<C-p>", [[:let @+=luaeval('require"jsonpath".get()')<CR>]])
