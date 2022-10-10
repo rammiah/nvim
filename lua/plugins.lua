bf76e62e521f59ed527b7e9c417846d24a9cad9d
rammiah <rammiahcn@gmail.com>
2022-10-10 23:44:35 +0800

    fix lspkind

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -184,7 +184,6 @@ return require("packer").startup({
         -- use "norcalli/nvim-colorizer.lua"
         use {
             "uga-rosa/ccc.nvim",
-            branch = "0.7.2",
             config = function()
                 require("ccc").setup {
                     highlighter = {
@@ -296,37 +295,32 @@ return require("packer").startup({
         use "phelipetls/jsonpath.nvim"
         use "hrsh7th/cmp-path"
         use "hrsh7th/cmp-cmdline"
-        use {
-            "onsails/lspkind.nvim",
-            config = function()
-                require("lspkind").init {
-                    mode = "symbol",
-                    preset = "codicons",
-                }
-            end,
-        }
+        use "hrsh7th/cmp-buffer"
+        use "onsails/lspkind.nvim"
         use {
             "hrsh7th/nvim-cmp",
             config = function()
                 local cmp = require "cmp"
                 local lspkind = require "lspkind"
                 cmp.setup {
-                    formating = {
-                        fields = { "kind", "abbr" },
-                        format = lspkind.cmp_format {
-                            with_text = false,
+                    formatting = {
+                        format = lspkind.cmp_format({
+                            preset = "codicons",
                             mode = "symbol", -- show only symbol annotations
                             maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
                             ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-                        },
+
+                            -- The function below will be called before any actual modifications from lspkind
+                            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                            before = function(entry, vim_item)
+                                return vim_item
+                            end
+                        })
                     },
-                    view = {
-                        entries = "custom",
-                    },
-                    preselect = cmp.PreselectMode.Item,
                 }
                 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
                 cmp.setup.cmdline({ "/", "?" }, {
+                    -- fields = { "kind", "abbr" },
                     mapping = cmp.mapping.preset.cmdline(),
                     sources = {
                         { name = "buffer", keyword_length = 2 }
@@ -335,6 +329,7 @@ return require("packer").startup({
 
                 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
                 cmp.setup.cmdline(":", {
+                    -- fields = { "kind", "abbr" },
                     mapping = cmp.mapping.preset.cmdline(),
                     sources = cmp.config.sources(
                         {
