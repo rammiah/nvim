56c1f57366b1ac07b5ae7d3cd6cd18d993652334
rammiah <rammiahcn@gmail.com>
2022-10-10 19:27:02 +0800

    add cmd cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -293,7 +293,61 @@ return require("packer").startup({
                 }
             end
         }
-        use  "phelipetls/jsonpath.nvim"
+        use "phelipetls/jsonpath.nvim"
+        use {
+            "hrsh7th/nvim-cmp",
+            requires = {
+                "hrsh7th/cmp-path",
+                "hrsh7th/cmp-cmdline",
+                "onsails/lspkind.nvim",
+            },
+            after = {
+                "lspkind",
+            },
+            config = function()
+                local cmp = require "cmp"
+                local lspkind = require "lspkind"
+                cmp.setup {
+                    formating = {
+                        fields = { "kind", "abbr" },
+                        format = lspkind.cmp_format {
+                            with_text = false,
+                            mode = "symbol", -- show only symbol annotations
+                            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                        },
+                    },
+                    preselect = cmp.PreselectMode.Item,
+                }
+                -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+                cmp.setup.cmdline({ "/", "?" }, {
+                    mapping = cmp.mapping.preset.cmdline(),
+                    sources = {
+                        { name = "buffer", keyword_length = 2 }
+                    }
+                })
+
+                -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+                cmp.setup.cmdline(":", {
+                    mapping = cmp.mapping.preset.cmdline(),
+                    sources = cmp.config.sources(
+                        {
+                            { name = "cmdline", keyword_length = 2 }
+                        }, {
+                        { name = "path", keyword_length = 2 }
+                    }),
+                })
+            end
+        }
+        use {
+            "onsails/lspkind.nvim",
+            config = function()
+                require("lspkind").init {
+                    mode = "symbol",
+                    preset = "codicons",
+                }
+            end,
+        }
     end,
     config = {
         ensure_dependencies  = true, -- Should packer install plugin dependencies?
