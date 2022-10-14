7b2baf37dfae1a5d975badc0c1c700afecf953d1
rammiah <rammiahcn@gmail.com>
2022-10-14 14:25:58 +0800

    add zsh plugin

--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -2,10 +2,12 @@ local map = require("localutils").KeyMap
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
 
--- jsonpath support
-if vim.fn.exists("+winbar") == 1 then
-    vim.wo.winbar = "%{luaeval('require\"jsonpath\".get()')}"
-end
 
--- send json path to clipboard
-map("n", "y<C-p>", [[:let @+=luaeval('require"jsonpath".get()')<CR>]])
+if require("localutils").safe_load("jsonpath") then
+    -- jsonpath support
+    if vim.fn.exists("+winbar") == 1 then
+        vim.wo.winbar = "%{luaeval('require\"jsonpath\".get()')}"
+    end
+    -- send json path to clipboard
+    map("n", "y<C-p>", [[:let @+=luaeval('require"jsonpath".get()')<CR>]])
+end
