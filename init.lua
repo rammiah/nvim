3b37b7a44d554309fe66288bf541473c76241bb7
rammiah <rammiahcn@gmail.com>
2022-10-17 11:30:06 +0800

    fix global

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 -- basic config
-_M = {} -- ad a global variable
 require("basic")
 require("filetype")
 require("commands")
