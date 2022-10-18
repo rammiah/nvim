cf7132bd1dcf3f17632754f30745992dfbc47e02
rammiah <rammiahcn@gmail.com>
2022-10-18 13:24:55 +0800

    vsplit help

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -403,6 +403,17 @@ return require("packer").startup({
                 "markdown"
             },
         }
+        use {
+            "anuvyklack/help-vsplit.nvim",
+            config = function()
+                require("help-vsplit").setup {
+                    always = false, -- Always open help in a vertical split.
+                    side = "right", -- 'left' or 'right'
+                    buftype = { "help" },
+                    filetype = { "man" }
+                }
+            end,
+        }
     end,
     config = {
         ensure_dependencies  = true, -- Should packer install plugin dependencies?
