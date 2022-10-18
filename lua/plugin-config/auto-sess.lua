93f85fd7de05129be1b8c35286f995874957fd52
rammiah <rammiahcn@gmail.com>
2022-10-18 17:50:14 +0800

    save session to state

--- lua/plugin-config/auto-sess.lua
+++ lua/plugin-config/auto-sess.lua
@@ -3,7 +3,7 @@ local map = require("localutils").KeyMap
 require("auto-session").setup {
     log_level = "info",
     auto_session_enable_last_session = false,
-    auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
+    auto_session_root_dir = vim.fn.stdpath("state") .. "/sessions/",
     auto_session_enabled = true,
     auto_save_enabled = false,
     auto_restore_enabled = false,
