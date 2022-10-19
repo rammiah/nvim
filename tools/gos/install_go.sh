70fc244e48e60c1bbce921f78ac97e5bdc7bd28a
wangyaning <wangyaning.ovo@bytedance.com>
2022-10-19 21:46:14 +0800

    update kitty, nvim

--- tools/gos/install_go.sh
+++ tools/gos/install_go.sh
@@ -4,44 +4,44 @@
 
 if [ ! -d "$HOME/.gos" ]
 then
-	echo "creating gos directory"
-	mkdir "$HOME/.gos" || echo "create $HOME/.gos fail"
+    echo "creating gos directory"
+    mkdir "$HOME/.gos" || echo "create $HOME/.gos fail"
 fi
 
 if [ ! -d "$HOME/.gos" ]
 then
-	echo "create $HOME/.gos fail"
+    echo "create $HOME/.gos fail"
 fi
 
 ARCH=""
 case $(uname -m) in
-	i386)   ARCH="386" ;;
-	i686)   ARCH="386" ;;
-	x86_64) ARCH="amd64" ;;
-	arm)    dpkg --print-ARCH | grep -q "arm64" && ARCH="arm64" || ARCH="arm" ;;
+    i386)   ARCH="386" ;;
+    i686)   ARCH="386" ;;
+    x86_64) ARCH="amd64" ;;	
+    arm64)  ARCH="arm64" ;;
+    arm)    dpkg --print-ARCH | grep -q "arm64" && ARCH="arm64" || ARCH="arm" ;;
 esac
 
 OS=""
 case $(uname -s) in
-	Darwin)	OS="darwin"	;;
-	Linux)	OS="linux"	;;
+    Darwin)	OS="darwin"	;;
+    Linux)	OS="linux"	;;
 esac
 
 VERSION=$1
 if [[ $VERSION =~ ^go* ]]
 then
-	VERSION=${VERSION:2}
+    VERSION=${VERSION:2}
 fi
 
 DOWN_FILE="/tmp/go$VERSION.tar.gz"
 
 cd $HOME/.gos &&\
-	wget -O "$DOWN_FILE" "https://golang.org/dl/go$VERSION.$OS-$ARCH.tar.gz" &&\
-	tar -xvf "$DOWN_FILE" -C /tmp/ &&\
-	rm -rf go$VERSION &&\
-	mv /tmp/go go$VERSION &&\
-	rm -rf "$DOWN_FILE" &&\
-	echo "download go$VERSION success" ||\
-	echo "download go$VERSION fail" &&\
-	rm -rf "$DOWN_FILE"
-
+    wget -O "$DOWN_FILE" "https://golang.org/dl/go$VERSION.$OS-$ARCH.tar.gz" &&\
+    tar -xvf "$DOWN_FILE" -C /tmp/ &&\
+    rm -rf go$VERSION &&\
+    mv /tmp/go go$VERSION &&\
+    rm -rf "$DOWN_FILE" &&\
+    echo "download go$VERSION success" ||\
+    echo "download go$VERSION fail" &&\
+    rm -rf "$DOWN_FILE"
