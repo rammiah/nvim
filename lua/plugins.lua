7919061bb1f084e9a6555adcf7636068d3727221
wangyaning <wangyaning.ovo@bytedance.com>
2022-10-21 14:31:44 +0800

    config undotree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -430,6 +430,10 @@ return require("packer").startup({
             "mbbill/undotree",
             config = function()
                 vim.g.undotree_WindowLayout = 3
+                vim.g.undotree_DiffAutoOpen = 1
+                vim.g.undotree_SetFocusWhenToggle = 1
+                vim.g.undotree_DiffCommand = "delta"
+                vim.g.undotree_ShortIndicators = 1
             end,
         }
         -- better fold
