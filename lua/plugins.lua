6423acac19eb425ed6404eebc53a3e258e4200b4
rammiah <rammiahcn@gmail.com>
2022-12-10 14:59:58 +0800

    fix undotree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -420,7 +420,7 @@ return require("packer").startup({
                 vim.g.undotree_WindowLayout = 3
                 vim.g.undotree_DiffAutoOpen = 1
                 vim.g.undotree_SetFocusWhenToggle = 1
-                vim.g.undotree_DiffCommand = "delta"
+                -- vim.g.undotree_DiffCommand = "delta"
                 vim.g.undotree_ShortIndicators = 1
             end,
         }
