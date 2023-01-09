52b6d92f8468cc35fbe3eac7b098a3fca3dfb345
rammiah <rammiahcn@gmail.com>
2023-01-09 20:44:15 +0800

    add banner

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
+-- https://patorjk.com/software/taag/#p=display&v=0&f=ANSI%20Shadow&t=VC%20CODE
+-- ██╗   ██╗███████╗     ██████╗ ██████╗ ██████╗ ███████╗
+-- ██║   ██║██╔════╝    ██╔════╝██╔═══██╗██╔══██╗██╔════╝
+-- ██║   ██║███████╗    ██║     ██║   ██║██║  ██║█████╗  
+-- ╚██╗ ██╔╝╚════██║    ██║     ██║   ██║██║  ██║██╔══╝  
+--  ╚████╔╝ ███████║    ╚██████╗╚██████╔╝██████╔╝███████╗
+--   ╚═══╝  ╚══════╝     ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝
 -- git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 -- basic config
 require("basic")
