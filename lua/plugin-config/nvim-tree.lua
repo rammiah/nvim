0c853cdc48c536e137c55f4c71567fe169b5faa0
rammiah <rammiahcn@gmail.com>
2023-05-05 19:02:09 +0800

    fix nvim-tree

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -1,6 +1,6 @@
 local map = require("localutils").KeyMap
 local lib = require("nvim-tree.lib")
-local view = require("nvim-tree.view")
+local api = require("nvim-tree.api")
 
 map("n", "<leader>e", "<Cmd>NvimTreeToggle<CR>")
 
@@ -13,7 +13,7 @@ local function edit_or_open()
     if node.link_to and not node.nodes then
         -- 链接到文件
         require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
-        view.close() -- Close the tree if file was opened
+        api.tree.close() -- Close the tree if file was opened
     elseif node.nodes ~= nil then
         -- 文件夹节点
         lib.expand_or_collapse(node)
@@ -23,7 +23,7 @@ local function edit_or_open()
         -- 文件节点
         -- print(vim.inspect(node))
         require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
-        view.close() -- Close the tree if file was opened
+        api.tree.close() -- Close the tree if file was opened
     end
 end
 
@@ -44,10 +44,11 @@ local function split_preview()
     end
 
     -- Finally refocus on tree if it was lost
-    if not view.is_visible() then
-        view.open()
+    if not api.tree.is_visible() then
+        api.tree.open()
     end
-    view.focus()
+    api.tree.focus()
+    api.tree.reload()
 end
 
 local function vsplit_preview()
@@ -67,10 +68,11 @@ local function vsplit_preview()
     end
 
     -- Finally refocus on tree if it was lost
-    if not view.is_visible() then
-        view.open()
+    if not api.tree.is_visible() then
+        api.tree.open()
     end
-    view.focus()
+    api.tree.focus()
+    api.tree.reload()
 end
 
 -- telescope node support
@@ -80,75 +82,76 @@ end
 
 
 local function on_attach(bufnr)
-  local api = require('nvim-tree.api')
+    local api = require('nvim-tree.api')
 
-  local function opts(desc)
-    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
-
-
-  -- Default mappings not inserted as:
-  --  remove_keymaps = true
-  --  OR
-  --  view.mappings.custom_only = true
+    local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
 
 
-  -- Mappings migrated from view.mappings.list
-  --
-  -- You will need to insert "your code goes here" for any mappings with a custom action_cb
-  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
-  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
-  vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
-  vim.keymap.set('n', 'O', api.node.open.no_window_picker, opts('Open: No Window Picker'))
-  vim.keymap.set('n', '<C-]>', api.tree.change_root_to_node, opts('CD'))
-  vim.keymap.set('n', '<2-RightMouse>', api.tree.change_root_to_node, opts('CD'))
-  vim.keymap.set('n', '<C-v>', api.node.open.vertical, opts('Open: Vertical Split'))
-  vim.keymap.set('n', '<C-x>', api.node.open.horizontal, opts('Open: Horizontal Split'))
-  vim.keymap.set('n', '<C-t>', api.node.open.tab, opts('Open: New Tab'))
-  vim.keymap.set('n', '<', api.node.navigate.sibling.prev, opts('Previous Sibling'))
-  vim.keymap.set('n', '>', api.node.navigate.sibling.next, opts('Next Sibling'))
-  vim.keymap.set('n', 'P', api.node.navigate.parent, opts('Parent Directory'))
-  vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
-  vim.keymap.set('n', '<Tab>', api.node.open.preview, opts('Open Preview'))
-  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
-  vim.keymap.set('n', 'L', function()
-    local node = api.tree.get_node_under_cursor()
-    -- your code goes here
-  end, opts('vsplit_preview'))
+    -- Default mappings not inserted as:
+    --  remove_keymaps = true
+    --  OR
+    --  view.mappings.custom_only = true
 
-  vim.keymap.set('n', 'J', function()
-    local node = api.tree.get_node_under_cursor()
-    -- your code goes here
-  end, opts('split_preview'))
 
-  vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
-  vim.keymap.set('n', 'I', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
-  vim.keymap.set('n', 'H', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
-  vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
-  vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
-  vim.keymap.set('n', 'D', api.fs.remove, opts('Delete'))
-  vim.keymap.set('n', 'd', api.fs.trash, opts('Trash'))
-  vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
-  vim.keymap.set('n', '<C-r>', api.fs.rename_sub, opts('Rename: Omit Filename'))
-  vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
-  vim.keymap.set('n', 'y', api.fs.copy.node, opts('Copy'))
-  vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
-  vim.keymap.set('n', 'c', api.fs.copy.filename, opts('Copy Name'))
-  vim.keymap.set('n', 'C', api.fs.copy.relative_path, opts('Copy Relative Path'))
-  vim.keymap.set('n', 'gy', api.fs.copy.absolute_path, opts('Copy Absolute Path'))
-  vim.keymap.set('n', '[h', api.node.navigate.git.prev, opts('Prev Git'))
-  vim.keymap.set('n', ']h', api.node.navigate.git.next, opts('Next Git'))
-  vim.keymap.set('n', '-', api.tree.change_root_to_parent, opts('Up'))
-  vim.keymap.set('n', 'f', api.live_filter.start, opts('Filter'))
-  vim.keymap.set('n', 'F', api.live_filter.clear, opts('Clean Filter'))
-  vim.keymap.set('n', 'q', api.tree.close, opts('Close'))
-  vim.keymap.set('n', 'W', api.tree.collapse_all, opts('Collapse'))
-  vim.keymap.set('n', 'E', api.tree.expand_all, opts('Expand All'))
-  vim.keymap.set('n', 'S', api.tree.search_node, opts('Search'))
-  vim.keymap.set('n', '.', api.node.run.cmd, opts('Run Command'))
-  vim.keymap.set('n', '<C-k>', api.node.show_info_popup, opts('Info'))
-  vim.keymap.set('n', 'g?', api.tree.toggle_help, opts('Help'))
+    -- Mappings migrated from view.mappings.list
+    --
+    -- You will need to insert "your code goes here" for any mappings with a custom action_cb
+    vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+    vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+    vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
+    vim.keymap.set('n', 'O', api.node.open.no_window_picker, opts('Open: No Window Picker'))
+    vim.keymap.set('n', '<C-]>', api.tree.change_root_to_node, opts('CD'))
+    vim.keymap.set('n', '<2-RightMouse>', api.tree.change_root_to_node, opts('CD'))
+    vim.keymap.set('n', '<C-v>', api.node.open.vertical, opts('Open: Vertical Split'))
+    vim.keymap.set('n', '<C-x>', api.node.open.horizontal, opts('Open: Horizontal Split'))
+    vim.keymap.set('n', '<C-t>', api.node.open.tab, opts('Open: New Tab'))
+    vim.keymap.set('n', '<', api.node.navigate.sibling.prev, opts('Previous Sibling'))
+    vim.keymap.set('n', '>', api.node.navigate.sibling.next, opts('Next Sibling'))
+    vim.keymap.set('n', 'P', api.node.navigate.parent, opts('Parent Directory'))
+    vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
+    vim.keymap.set('n', '<Tab>', api.node.open.preview, opts('Open Preview'))
 
+    vim.keymap.set('n', 'l', function()
+        edit_or_open()
+    end, opts('Open'))
+
+    vim.keymap.set('n', 'L', function()
+        vsplit_preview()
+    end, opts('vsplit_preview'))
+
+    vim.keymap.set('n', 'J', function()
+        split_preview()
+    end, opts('split_preview'))
+
+    vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+    vim.keymap.set('n', 'I', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
+    vim.keymap.set('n', 'H', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
+    vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
+    vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
+    vim.keymap.set('n', 'D', api.fs.remove, opts('Delete'))
+    vim.keymap.set('n', 'd', api.fs.trash, opts('Trash'))
+    vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
+    vim.keymap.set('n', '<C-r>', api.fs.rename_sub, opts('Rename: Omit Filename'))
+    vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
+    vim.keymap.set('n', 'y', api.fs.copy.node, opts('Copy'))
+    vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
+    vim.keymap.set('n', 'c', api.fs.copy.filename, opts('Copy Name'))
+    vim.keymap.set('n', 'C', api.fs.copy.relative_path, opts('Copy Relative Path'))
+    vim.keymap.set('n', 'gy', api.fs.copy.absolute_path, opts('Copy Absolute Path'))
+    vim.keymap.set('n', '[h', api.node.navigate.git.prev, opts('Prev Git'))
+    vim.keymap.set('n', ']h', api.node.navigate.git.next, opts('Next Git'))
+    vim.keymap.set('n', '-', api.tree.change_root_to_parent, opts('Up'))
+    vim.keymap.set('n', 'f', api.live_filter.start, opts('Filter'))
+    vim.keymap.set('n', 'F', api.live_filter.clear, opts('Clean Filter'))
+    vim.keymap.set('n', 'q', api.tree.close, opts('Close'))
+    vim.keymap.set('n', 'W', api.tree.collapse_all, opts('Collapse'))
+    vim.keymap.set('n', 'E', api.tree.expand_all, opts('Expand All'))
+    vim.keymap.set('n', 'S', api.tree.search_node, opts('Search'))
+    vim.keymap.set('n', '.', api.node.run.cmd, opts('Run Command'))
+    vim.keymap.set('n', '<C-k>', api.node.show_info_popup, opts('Info'))
+    vim.keymap.set('n', 'g?', api.tree.toggle_help, opts('Help'))
 end
 
 -- setup with all defaults
