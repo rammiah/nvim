de206a1fe77bfa747bf42cfa311114d6b6c0a368
rammiah <rammiahcn@gmail.com>
2023-05-08 18:08:57 +0800

    fix lazy

--- lua/plugin-config/auto-sess.lua
+++ lua/plugin-config/auto-sess.lua
@@ -12,12 +12,3 @@ session.setup {
     -- the configs below are lua only
     bypass_session_save_file_types = { "NvimTree", "thrift", "diff" }
 }
-
-local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<leader>ss", function()
-    session.SaveSession()
-end, opts)
-
-vim.keymap.set("n", "<leader>sr", function()
-    session.RestoreSession()
-end, opts)
