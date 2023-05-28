e4db3ba5059ff0bba8886e42e68b0afde6021b33
rammiah <rammiahcn@gmail.com>
2023-05-29 00:57:00 +0800

    fix nvim-go

--- ftplugin/go/go.lua
+++ ftplugin/go/go.lua
@@ -3,11 +3,11 @@ vim.bo.shiftwidth = 4
 vim.bo.expandtab = false
 
 local Map = require("localutils").KeyMap
-if require("localutils").safe_load("go") then
-    Map("n", "<leader>tf", ":GoTestFunc<cr>", {
-        silent = false,
-    })
-    Map("n", "<leader>tF", ":GoTestFile<cr>", {
-        silent = false,
-    })
-end
+-- if require("localutils").safe_load("go") then
+Map("n", "<leader>tf", ":GoTestFunc<cr>", {
+    silent = false,
+})
+Map("n", "<leader>tF", ":GoTestFile<cr>", {
+    silent = false,
+})
+-- end
