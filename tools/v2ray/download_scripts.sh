9f187b16c9685a08e5c5024ac577b679521a7ec2
rammiah <rammiahcn@gmail.com>
2023-10-19 16:21:31 +0800

    remove unused tools

--- tools/v2ray/download_scripts.sh
+++
@@ -1,8 +0,0 @@
-#/usr/bin/env bash
-
-# 安裝執行檔和 .dat 資料檔
-wget -O install-release.sh https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
-# 只更新 .dat 資料檔
-wget -O install-dat-release.sh https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
-
-chmod +x install-release.sh install-dat-release.sh
