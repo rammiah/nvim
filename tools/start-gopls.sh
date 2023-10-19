45d8bf4fb8736731ac8e9666c0deffb2b8f0684d
rammiah <rammiahcn@gmail.com>
2023-10-19 16:21:31 +0800

    remove unused tools

--- tools/start-gopls.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-rm -rf /tmp/gopls-daemon-socket
-# source /data00/home/wangyaning.ovo/.zshrc
-gopls -listen="unix;/tmp/gopls-daemon-socket" -logfile=auto -debug=""
-rm -rf /tmp/gopls-daemon-socket
