ae9f3225297139fe730af3d58e6b3c68295e835e
rammiah <rammiahcn@gmail.com>
2023-12-01 19:26:32 +0800

    use git proxy

--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ if not vim.loop.fs_stat(lazypath) then
         "git",
         "clone",
         "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
+        "https://gh.i2x.cc/github.com/folke/lazy.nvim.git",
         "--branch=stable", -- latest stable release
         lazypath,
     })
