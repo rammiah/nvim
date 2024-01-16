9e58465d11293b9a84e2387e406a2baa41a6ec69
rammiah <rammiahcn@gmail.com>
2024-01-16 10:28:27 +0800

    fix alacritty c-6

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -5,6 +5,7 @@ map('v', '>', '>gv')
 map("n", "]q", ":cnext<CR>")
 map("n", "[q", ":cprevious<CR>")
 map("n", "<C-s>", "<Cmd>noautocmd write<CR>")
+map("n", "<C-6>", "<C-^>")
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>ss", function()
