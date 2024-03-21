600a3a6bcc60388686605b15e5bba9c07fe22f89
rammiah <rammiahcn@gmail.com>
2024-03-21 14:27:07 +0800

    fix E31

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -98,14 +98,6 @@ require("nvim-treesitter.configs").setup {
     indent = {
         enable = true,
     },
-    rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true,  -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = 5000, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
-    },
     auto_tag = {
         enable = true,
     },
