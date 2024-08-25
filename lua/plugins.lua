27e1b2dfa24cc71dde6e7471553bd8666331930a
rammiah <rammiahcn@gmail.com>
2024-08-25 21:44:21 +0800

    set treesitter commit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -93,7 +93,8 @@ require("lazy").setup({
         },
         lazy = true,
         event = "VeryLazy",
-        commit = "495c93b0d4f49f688a83f493bee0908fe1d2d2df",
+        -- commit = "495c93b0d4f49f688a83f493bee0908fe1d2d2df",
+        commit = "bef7ec6",
         config = function()
             vim.g.skip_ts_context_commentstring_module = true
             require("plugin-config.nvim-treesitter")
