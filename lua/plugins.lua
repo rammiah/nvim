0b4ec128ad6942f22a4afffa6f54fbf6f9f44bd3
rammiah <rammiahcn@gmail.com>
2024-11-27 16:43:12 +0800

    update codeium

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -692,16 +692,17 @@ require("lazy").setup({
         },
     },
     {
-        "Exafunction/codeium.vim",
+        "rammiah/codeium.vim",
         config = function()
             vim.g.codeium_idle_delay = 500
             vim.g.codeium_disable_bindings = 1
+            -- vim.g.codeium_log_file = "/tmp/codeium.log"
+            -- vim.g.codeium_log_level = "DEBUG"
             vim.keymap.set('i', '<Tab>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
         end,
         lazy = true,
         event = "BufEnter",
-        -- version = "1.20.8",
-        version = "1.12.0",
+        version = "1.28.3",
     },
     {
         "akinsho/git-conflict.nvim",
