1ca4ab27ea7812017ac78a6f81980443eb4fecf3
rammiah <rammiahcn@gmail.com>
2025-04-03 21:00:20 +0800

    update plugins

---
+++ after/ftplugin/NvimTree.lua
@@ -0,0 +1,14 @@
+require("nvim-surround").buffer_setup {
+    keymaps = {
+        insert = false,
+        insert_line = false,
+        normal = false,
+        normal_cur = false,
+        normal_line = false,
+        normal_cur_line = false,
+        visual = false,
+        visual_line = false,
+        delete = false,
+        change = false,
+    },
+}
