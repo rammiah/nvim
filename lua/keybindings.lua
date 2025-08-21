a07ba3b1b4d4969fcf837bcb2766568d14333c5d
rammiah <rammiahcn@gmail.com>
2025-08-21 14:10:47 +0800

    update code

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -4,7 +4,7 @@ map('v', '<', '<gv')
 map('v', '>', '>gv')
 map("n", "]q", ":cnext<CR>")
 map("n", "[q", ":cprevious<CR>")
-map("n", "<C-s>", "<Cmd>noautocmd write<CR>")
+map("n", "<C-s>", "<Cmd>noautocmd write!<CR>")
 -- map("n", "<C-6>", "<C-^>")
 
 local opts = { noremap = true, silent = true }
