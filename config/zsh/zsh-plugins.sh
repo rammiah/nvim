4fc295466d68c64dd7e6f4c0f38fa80cb3614736
rammiah <rammiahcn@gmail.com>
2026-03-03 10:22:09 +0800

    update zsh-plugins.sh

--- config/zsh/zsh-plugins.sh
+++ config/zsh/zsh-plugins.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-function clone_or_pull() {
-    test -e "$2" && cd "$2" && git pull || git clone "$1" "$2"
+function clone_repo() {
+    test -e "$2" && rm -rf "$2"
+    git clone "$1" "$2"
 }
 
-clone_or_pull https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-clone_or_pull https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-clone_or_pull https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
-clone_or_pull https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-clone_or_pull https://github.com/TamCore/autoupdate-oh-my-zsh-plugins ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autoupdate
+clone_repo https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+clone_repo https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+clone_repo https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+clone_repo https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+clone_repo https://github.com/TamCore/autoupdate-oh-my-zsh-plugins ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autoupdate
 
 mkdir -p ~/.zshconf/post
 cp -f ~/.config/nvim/config/zsh/clipcopy.zsh ~/.zshconf/post/
